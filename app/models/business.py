d7d89d14c8821af11f49d97ecc3b6118e6e8f9e6
LuisCarrilloF <ingluiscf@gmail.com>
2026-09-23 13:10:48 -0600

    fix: se corrigio donde se guardan los negocios

--- app/models/business.py
+++ app/models/business.py
@@ -4,7 +4,9 @@ class Business:
         name: str,
         description: str,
         logo: str,
+        services: list[str] | None = None,
     ):
         self.name = name
         self.description = description
         self.logo = logo
+        self.services = services or []
